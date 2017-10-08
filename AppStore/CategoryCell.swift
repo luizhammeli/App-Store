b9a9ae19af48b85076f820d723a0a790dde35ee7
Luiz Hammerli <luizhammerli@MacBook-Pro-de-Luiz.local>
2017-10-08 09:33:41 -0300

    first commit

---
+++ AppStore/CategoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCell.swift
+//  AppStore
+//
+//  Created by Luiz Hammerli on 19/05/17.
+//  Copyright © 2017 Luiz Hammerli. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+}
---
+++ AppStore/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  AppStore
+//
+//  Created by Luiz Hammerli on 21/05/17.
+//  Copyright © 2017 Luiz Hammerli. All rights reserved.
+//
+
+import Foundation
